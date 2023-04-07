ec64a00b1566cf7f7ce3a8ee49bc020c756f108d
A-Amaram <104218950+A-Amaram@users.noreply.github.com>
2023-04-07 13:25:22 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 // Include any header files you write here:
 // (ex: #include <iostream>)
-
+#include <iostream>
 // Include your `main` function here.
+using namespace std;
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
